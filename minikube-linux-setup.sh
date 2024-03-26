4b5a08b908ece2106ca00edc9f6b616ecd61f33c
es <eryk.stando@gmail.com>
2024-03-26 18:55:15 +0100

    [setup] Add basic shell script for setting up local dev env

---
+++ minikube-linux-setup.sh
@@ -0,0 +1,22 @@
+# #/bin/bash
+set -e
+
+minikube start
+
+if minikube status | grep -q "host: Running" && minikube status | grep -q "kubelet: Running"; then
+    echo "Minikube has successfully started."
+else
+    echo "Minikube failed to start."
+fi
+
+minikube addons enable ingress
+
+#remove old entries
+sudo sed -i '/hms\.test\|auth\.hms\.test/d' /etc/hosts
+
+MINIKUBE_IP=$(minikube ip)
+
+#add new entries
+echo "$MINIKUBE_IP auth.hms.test hms.test" | sudo tee -a /etc/hosts
+
+echo "Minikube setup complete."
