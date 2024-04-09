5cb72ce8f7501bfa79fbd6afe8c582206be4a5ba
Eryk Stańdo <eryk.stando@gmail.com>
2024-04-09 14:59:50 +0200

    [client] i hate frontend technologies

--- client/src/auth/keycloak.jsx
+++ client/src/auth/keycloak.jsx
@@ -1,9 +1,11 @@
 import Keycloak from "keycloak-js";
-
 const keycloak = new Keycloak({
-    url: process.env.KEYCLOAK_SERVER_URL,
-    realm: process.env.REALM_NAME,
-    clientId: process.env.CLIENT_ID,
+    //i hate JS, HTML, REACT, VITE, and all of this
+    //react/vite/js cant directly read system envs 
+    //TODO: build no-dev image without VITE etc and use envs from system
+    url: "https://auth.hms.test.boredotter.dev",
+    realm: "hms",
+    clientId: "hms",
 });
 
 export default keycloak;
