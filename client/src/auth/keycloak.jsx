5c11971c666a16571d585443869ceb2298c8db8a
Eryk Stańdo <eryk.stando@gmail.com>
2024-04-09 14:52:59 +0200

    [deployment] wip; [client] try to get KC config from envs

--- client/src/auth/keycloak.jsx
+++ client/src/auth/keycloak.jsx
@@ -1,8 +1,9 @@
 import Keycloak from "keycloak-js";
+
 const keycloak = new Keycloak({
-    url: "https://auth.hms.test",
-    realm: "hms",
-    clientId: "hms",
+    url: process.env.KEYCLOAK_SERVER_URL,
+    realm: process.env.REALM_NAME,
+    clientId: process.env.CLIENT_ID,
 });
 
 export default keycloak;
